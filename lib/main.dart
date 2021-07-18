06dfbfafcd2f778633e8dd0afcfacaea19484fa7
Czappivagyok <czappivagyok@gmail.com>
2021-07-18 23:26:54 +0200

    :construction: Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:artifactproject/src/api/ManganatoAPI.dart';
 import 'package:artifactproject/src/bloc/MangaList/HotMangaListBloc.dart';
+import 'package:artifactproject/src/bloc/MangaList/LatestMangaListBloc.dart';
+import 'package:artifactproject/src/bloc/MangaList/NewestMangaListBloc.dart';
 import 'package:artifactproject/src/bloc/MangaPage/MangaPage.dart';
 import 'package:artifactproject/src/providers/NavigationProvider.dart';
 import 'package:artifactproject/src/providers/SettingsProvider.dart';
@@ -7,6 +9,7 @@ import 'package:artifactproject/src/translations/Translation.dart';
 import 'package:artifactproject/src/ui/Homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 import 'package:get/get.dart';
 
@@ -23,13 +26,15 @@ class Providers extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        Provider(create: (ctx) => ManganatoAPI()),
+        Provider(create: (context) => ManganatoAPI()),
         ChangeNotifierProvider(create: (context) => SettingsProvider(context)),
         Provider(create: (context) => NavigationProvider(context)),
       ],
       child: MultiBlocProvider(
         providers: [
           BlocProvider(create: (context) => HotMangaListBloc(context)),
+          BlocProvider(create: (context) => NewestMangaListBloc(context)),
+          BlocProvider(create: (context) => LatestMangaListBloc(context)),
           BlocProvider(create: (context) => MangaPageBloc(context)),
         ],
         child: child,
@@ -44,10 +49,16 @@ class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Providers(
-      child: GetMaterialApp(
-        debugShowCheckedModeBanner: false,
-        translations: ArtifactTranslations(),
-        home: const Homepage(),
+      child: ScreenUtilInit(
+        designSize: const Size(411.4, 891.4),
+        builder: () => GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          translations: ArtifactTranslations(),
+          theme: ThemeData(
+            visualDensity: VisualDensity.standard,
+          ),
+          home: const Homepage(),
+        ),
       ),
     );
   }
