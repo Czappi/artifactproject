3aec3d4aa9c81bbd06b64403aec87afcbc6b2d8a
Czappivagyok <czappivagyok@gmail.com>
2021-07-21 20:04:38 +0200

    :art: Add HomepageElement widget

---
+++ lib/src/widgets/HomepageElement.dart
@@ -0,0 +1,112 @@
+import 'package:get/get.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter/material.dart';
+import 'package:artifactproject/src/providers/SettingsProvider.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
+
+class HomepageElement extends StatelessWidget {
+  final String title, subtitle;
+  final GestureTapCallback? onShowMore;
+  final Widget child;
+  const HomepageElement({
+    Key? key,
+    required this.title,
+    required this.child,
+    this.subtitle = "",
+    this.onShowMore,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 70.h,
+          padding: const EdgeInsets.fromLTRB(15, 12, 15, 12),
+          child: Row(
+            children: [
+              _TitleSubtitle(
+                title: title,
+                subtitle: subtitle,
+              ),
+              _ShowMore(
+                onShowMore: onShowMore,
+              ),
+            ],
+          ),
+        ),
+        child,
+      ],
+    );
+  }
+}
+
+class _ShowMore extends StatelessWidget {
+  final GestureTapCallback? onShowMore;
+  const _ShowMore({Key? key, this.onShowMore}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (onShowMore != null) {
+      return Align(
+        alignment: Alignment.bottomRight,
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: onShowMore,
+            child: Row(
+              children: [
+                Text(
+                  "#showmore".tr.capitalizeFirst!,
+                  style: context.atheme.mlsubtitleTextStyle,
+                ),
+                SizedBox(
+                  width: 3.w,
+                ),
+                Icon(
+                  PhosphorIcons.caretRightBold,
+                  color: context.atheme.iconColor,
+                )
+              ],
+            ),
+          ),
+        ),
+      );
+    } else {
+      return const SizedBox();
+    }
+  }
+}
+
+class _TitleSubtitle extends StatelessWidget {
+  final String title, subtitle;
+  const _TitleSubtitle({
+    Key? key,
+    required this.title,
+    required this.subtitle,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: context.atheme.mltitleTextStyle,
+          ),
+          Expanded(
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                subtitle,
+                style: context.atheme.mlsubtitleTextStyle,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
